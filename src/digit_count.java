26672d76d14c1d4b6cd03d4c53944246abd5695c
Dhikshanth-04 <squadattack9@gmail.com>
2026-01-17 15:20:53 +0530

    Count of digit

---
+++ src/digit_count.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class digit_count {
+    static int count(int n){
+        int count = 0;
+        int d = 0;
+        while(n>0){
+            d = n%10;
+            count++;
+            n = n/10;
+        }
+        return count;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(count(n));
+    }
+}
