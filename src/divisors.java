f57e3412813761b0bb14e4c8dcb25cae6b80cbfa
Dhikshanth-04 <squadattack9@gmail.com>
2026-01-20 11:15:19 +0530

    Divisors of the number

---
+++ src/divisors.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class divisors {
+    
+    static void divide(int n){
+        int i=0;
+        for(i=1;i*i<=n;i++){
+            if(n%i==0){
+                System.out.println(i);
+            }
+        }
+        for( ;i>=1;i--){
+            if(n%i==0  && i!=n/i){
+                System.out.println(n/i);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        divide(n);
+    }
+}
