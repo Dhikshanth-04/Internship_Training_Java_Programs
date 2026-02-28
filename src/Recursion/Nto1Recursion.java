c3ac47ba8e633fcdaab5de0b089f177555212725
Dhikshanth-04 <squadattack9@gmail.com>
2026-02-28 10:35:52 +0530

    recursion package and 1st program has added

---
+++ src/Recursion/Nto1Recursion.java
@@ -0,0 +1,17 @@
+package Recursion;
+import java.util.*;
+public class Nto1Recursion {
+    static void fun(int n){
+        if(n<1){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+    
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        fun(n);
+    }
+}
