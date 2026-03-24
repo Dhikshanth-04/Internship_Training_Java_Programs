0ce93d88c838bb0f04ad76d78084ec05b5a77ae7
Dhikshanth-04 <squadattack9@gmail.com>
2026-03-24 08:58:38 +0530

    without using built in methods

--- src/AscendingOrderUniqueChar.java
+++ src/AscendingOrderUniqueChar.java
@@ -1,11 +1,18 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class AscendingOrderUniqueChar {
     public static String ascendingOrderUniqueChar(String res){
         String t = "";
         char[] arr = res.toCharArray();
-        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]<arr[j]){
+                    char temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
         t = new String(arr);
         return t;
     } 
